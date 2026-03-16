0115f8fc1c7a34689243d2fd3a97df774a7d36f1
shressi10-png <shressi10@gmail.com>
2026-03-16 17:27:24 +0530

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -1,72 +1,87 @@
-const lessons=[
+const lessons = [
 
 {
-letters:["ಕ","ಗ"],
-sounds:["ka","ga"],
-hindi:["क","ग"],
+name:"Vowels 1",
+letters:["ಅ","ಆ"],
+sounds:["a","aa"],
+hindi:["अ","आ"],
+tamil:["அ","ஆ"],
+examples:[
+{word:"ಅಮ್ಮ",meaning:"mother",breakdown:"ಅ + ಮ್ಮ",phonetic:"a + mma"},
+{word:"ಆನೆ",meaning:"elephant",breakdown:"ಆ + ನೆ",phonetic:"aa + ne"}
+]
+},
+
+{
+name:"Vowels 2",
+letters:["ಇ","ಈ"],
+sounds:["i","ee"],
+hindi:["इ","ई"],
+tamil:["இ","ஈ"],
+examples:[
+{word:"ಇಲಿ",meaning:"rat",breakdown:"ಇ + ಲಿ",phonetic:"i + li"},
+{word:"ಈರುಳ್ಳಿ",meaning:"onion",breakdown:"ಈ + ರು + ಳ್ಳಿ",phonetic:"ee + ru + lli"}
+]
+},
+
+{
+name:"Velar 1",
+letters:["ಕ","ಖ"],
+sounds:["ka","kha"],
+hindi:["क","ख"],
 tamil:["க","க"],
-
 examples:[
-{
-word:"ಕಾನೂನು",
-meaning:"law",
-breakdown:"ಕಾ + ನೂ + ನು",
-phonetic:"kaa + noo + nu"
-},
-{
-word:"ಗಡಿ",
-meaning:"border",
-breakdown:"ಗ + ಡಿ",
-phonetic:"ga + di"
-}
+{word:"ಕಲ್ಲು",meaning:"stone",breakdown:"ಕ + ಲ್ಲು",phonetic:"ka + llu"},
+{word:"ಖಡ್ಗ",meaning:"sword",breakdown:"ಖ + ಡ್ಗ",phonetic:"kha + dga"}
 ]
-
 },
 
 {
-letters:["ತ","ದ"],
-sounds:["ta","da"],
-hindi:["त","द"],
-tamil:["த","த"],
-
+name:"Velar 2",
+letters:["ಗ","ಘ"],
+sounds:["ga","gha"],
+hindi:["ग","घ"],
+tamil:["க","க"],
 examples:[
-{
-word:"ತಪ್ಪು",
-meaning:"mistake",
-breakdown:"ತ + ಪ್ಪು",
-phonetic:"ta + ppu"
-},
-{
-word:"ದಾರಿ",
-meaning:"path",
-breakdown:"ದಾ + ರಿ",
-phonetic:"daa + ri"
-}
+{word:"ಗಡಿ",meaning:"border",breakdown:"ಗ + ಡಿ",phonetic:"ga + di"},
+{word:"ಘಂಟೆ",meaning:"bell",breakdown:"ಘ + ಣ್ಟೆ",phonetic:"gha + nte"}
 ]
-
 },
 
 {
+name:"Labial",
 letters:["ಪ","ಬ"],
 sounds:["pa","ba"],
 hindi:["प","ब"],
 tamil:["ப","ப"],
-
 examples:[
-{
-word:"ಪತ್ರ",
-meaning:"letter",
-breakdown:"ಪ + ತ್ರ",
-phonetic:"pa + tra"
-},
-{
-word:"ಬಾಳೆ",
-meaning:"banana",
-breakdown:"ಬಾ + ಳೆ",
-phonetic:"baa + le"
-}
+{word:"ಪತ್ರ",meaning:"letter",breakdown:"ಪ + ತ್ರ",phonetic:"pa + tra"},
+{word:"ಬಾಳೆ",meaning:"banana",breakdown:"ಬಾ + ಳೆ",phonetic:"baa + le"}
 ]
+},
 
+{
+name:"Dental",
+letters:["ತ","ದ"],
+sounds:["ta","da"],
+hindi:["त","द"],
+tamil:["த","த"],
+examples:[
+{word:"ತಪ್ಪು",meaning:"mistake",breakdown:"ತ + ಪ್ಪು",phonetic:"ta + ppu"},
+{word:"ದಾರಿ",meaning:"path",breakdown:"ದಾ + ರಿ",phonetic:"daa + ri"}
+]
+},
+
+{
+name:"Nasals",
+letters:["ಮ","ನ"],
+sounds:["ma","na"],
+hindi:["म","न"],
+tamil:["ம","ந"],
+examples:[
+{word:"ಮನೆ",meaning:"house",breakdown:"ಮ + ನೆ",phonetic:"ma + ne"},
+{word:"ನದಿ",meaning:"river",breakdown:"ನ + ದಿ",phonetic:"na + di"}
+]
 }
 
 ]
