5e6a7d069683e271aae58c405e024befe93e90ba
shressi10-png <shressi10@gmail.com>
2026-03-16 13:02:04 +0530

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -6,7 +6,9 @@ sounds:["ka","ga"],
 hindi:["क","ग"],
 tamil:["க","க"],
 examples:["ಕಾನೂನು","ಗಡಿ"],
-meanings:["law","border"]
+meanings:["law","border"],
+breakdown:["ಕಾ + ನೂ + ನು","ಗ + ಡಿ"],
+phonetic:["kaa + noo + nu","ga + di"]
 },
 
 {
@@ -15,7 +17,9 @@ sounds:["ta","da"],
 hindi:["त","द"],
 tamil:["த","த"],
 examples:["ತಪ್ಪು","ದಾರಿ"],
-meanings:["mistake","path"]
+meanings:["mistake","path"],
+breakdown:["ತ + ಪ್ಪು","ದಾ + ರಿ"],
+phonetic:["ta + ppu","daa + ri"]
 },
 
 {
@@ -24,34 +28,9 @@ sounds:["pa","ba"],
 hindi:["प","ब"],
 tamil:["ப","ப"],
 examples:["ಪತ್ರ","ಬಾಳೆ"],
-meanings:["letter","banana"]
-},
-
-{
-letters:["ನ","ಮ"],
-sounds:["na","ma"],
-hindi:["न","म"],
-tamil:["ந","ம"],
-examples:["ನದಿ","ಮನೆ"],
-meanings:["river","house"]
-},
-
-{
-letters:["ಯ","ರ"],
-sounds:["ya","ra"],
-hindi:["य","र"],
-tamil:["ய","ர"],
-examples:["ಯಾನ","ರಾತ್ರಿ"],
-meanings:["journey","night"]
-},
-
-{
-letters:["ಲ","ವ"],
-sounds:["la","va"],
-hindi:["ल","व"],
-tamil:["ல","வ"],
-examples:["ಲೋಕ","ವಾಹನ"],
-meanings:["world","vehicle"]
+meanings:["letter","banana"],
+breakdown:["ಪ + ತ್ರ","ಬಾ + ಳೆ"],
+phonetic:["pa + tra","baa + le"]
 }
 
 ]
